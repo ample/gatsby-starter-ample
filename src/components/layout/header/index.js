f09944f1a3f40f5c7b07b084cbf8d31cc32cf965
Sean C Davis <scdavis41@gmail.com>
2019-11-15 07:30:53 -0500

    Header and footer index file for importing/exporting only

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -1,17 +1,1 @@
-import React from "react"
-// import PropTypes from "prop-types"
-import { Link } from "gatsby"
-
-const Header = () => (
-  <header>
-    This is the header, and it has{" "}
-    <Link to="/">a link that will get you home</Link>.
-    <hr />
-  </header>
-)
-
-Header.propTypes = {}
-
-Header.defaultProps = {}
-
-export default Header
+export { default } from "./header"
