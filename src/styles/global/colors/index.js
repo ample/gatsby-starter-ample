91afd3fb0f7974b62677c6be7911b679721bb229
Jeff Lay <phritolay@icloud.com>
2020-02-25 16:00:14 -0500

    add global colors and docs for styleguide section
    upgrade sass

--- src/styles/global/colors/index.js
+++ src/styles/global/colors/index.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React from "react"
 
-import colors from './_index.scss'
+import colors from "./_index.scss"
+import styles from "./styles.module.scss"
 
-const Color = () => {
+const Colors = () => {
   const colorGrid = Object.keys(colors).map((key, index) => (
     <div
       key={index}
-      className="color"
+      className={styles.color}
       style={{
         backgroundColor: colors[key]
       }}
     >
-      <span>${key.replace('_', '-')}</span>
+      <span>
+        --
+        {key
+          .replace(/([A-Z]+)*([A-Z][a-z])/g, "$1 $2")
+          .split(" ")
+          .join("-")
+          .toLowerCase()}
+      </span>
     </div>
   ))
 
-  return <div className="colors">{colorGrid}</div>
+  return <div className={styles.colors}>{colorGrid}</div>
 }
 
-export default Color
+export default Colors
