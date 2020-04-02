dc7ecf619bf65a48caf1c6ba8aee357eea6e807c
Jeff Lay <phritolay@icloud.com>
2020-04-02 17:42:00 -0400

    revert gatsby-config.js sass configurations
    moving gatsby-theme-ample-componets to storybook for confilcts

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,6 +60,39 @@ module.exports = {
         }
       }
     },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        implementation: require("sass"),
+        sourceMap: true,
+        cssLoaderOptions: {
+          localIdentName: "[local]-[hash:base64:3]",
+          sourceMap: true
+        },
+        postCssPlugins: [
+          require("postcss-normalize"),
+          require("postcss-responsive-type"),
+          require("postcss-pxtorem")({
+            mediaQuery: true,
+            propWhiteList: [],
+            replace: true,
+            rootValue: 16
+          }),
+          require("postcss-preset-env")({
+            features: {
+              "custom-properties": {
+                preserve: true,
+                warnings: true
+              }
+            },
+            stage: 2
+          }),
+          require("autoprefixer")({
+            grid: "autoplace"
+          })
+        ]
+      }
+    },
     `gatsby-plugin-netlify`
   ]
 }
