0d2c39c7335e79d174049d0f49ceb44948355fa5
Sean C Davis <scdavis41@gmail.com>
2020-04-08 16:57:04 -0400

    [#40] Add google analytics support

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,6 +93,12 @@ module.exports = {
         ]
       }
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID
+      }
+    },
     `gatsby-plugin-netlify`
   ]
 }
