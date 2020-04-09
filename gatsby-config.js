870ee0054841017f94415affc331e8515d63227d
Sean C Davis <scdavis41@gmail.com>
2020-04-09 13:12:15 -0400

    [#40] Move sitemap plugin and test it

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,6 @@ module.exports = {
     `gatsby-theme-ample-components`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-catch-links`,
-    {
-      resolve: `gatsby-plugin-sitemap`,
-      options: {
-        output: `/sitemap.xml`
-      }
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -95,7 +89,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID
+        trackingId: ""
+      }
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/sitemap.xml`
       }
     },
     `gatsby-plugin-netlify`
