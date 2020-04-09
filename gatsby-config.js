a5b39312058eff553bd1e9ac251a36244a0b28c8
Sean C Davis <scdavis41@gmail.com>
2020-04-09 16:13:58 -0400

    Add ample defaults to manifest This also sets the favicon to the ample icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,13 +21,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-starter-ample`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#0095df`,
+        theme_color: `#0095df`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/ample-icon.png`
       }
     },
     {
