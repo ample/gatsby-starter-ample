51a32648543be83441e18334d12c24fbe92f78d7
Sean C Davis <scdavis41@gmail.com>
2020-07-23 12:10:55 -0400

    Add explicitly defined types to SEO plugin

---
+++ plugins/gatsby-ample-seo/gatsby-node.js
@@ -0,0 +1,33 @@
+/**
+ * Explicitly define type definitions for seo objects, so consistent fragments
+ * can be easily used throughout the site.
+ *
+ * Fragments are also included in this plugin so they can be reused throughout
+ * the project.
+ */
+exports.createSchemaCustomization = ({ actions }) => {
+  const typeDefs = `
+    type SeoMetaOg @infer {
+      description: String
+      image: File @fileByRelativePath
+      title: String
+    }
+
+    type SeoMetaTwitter @infer {
+      card: String
+      description: String
+      image: File @fileByRelativePath
+      title: String
+    }
+
+    type SeoMeta implements Node @infer {
+      description: String
+      image: File @fileByRelativePath
+      og: SeoMetaOg
+      title: String
+      twitter: SeoMetaTwitter
+    }
+  `
+
+  actions.createTypes(typeDefs)
+}
