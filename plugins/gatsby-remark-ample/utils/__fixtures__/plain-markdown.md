49774e0e854051a3799b4b473418d38733a936f0
Taylor C. MacDonald <taylor@helloample.com>
2020-09-01 15:27:59 -0400

    Automatically apply target=_blank to external links

--- plugins/gatsby-remark-ample/utils/__fixtures__/plain-markdown.md
+++ plugins/gatsby-remark-ample/utils/__fixtures__/plain-markdown.md
@@ -1,3 +1,3 @@
 # Hello World
 
-Voluptate aute minim nisi non sint nisi sint deserunt.
+Voluptate aute [minim nisi](https://ample.co) non sint [nisi sint](/home) deserunt.
--- plugins/gatsby-remark-ample/utils/process-markdown.js
+++ plugins/gatsby-remark-ample/utils/process-markdown.js
@@ -1,8 +1,25 @@
 const remark = require("remark")
 const remarkHTML = require("remark-html")
+const visit = require("unist-util-visit")
 
-module.exports = markdown =>
+module.exports = (markdown) =>
   remark()
     .use(remarkHTML)
+    .use(() => (tree) =>
+      visit(tree, "link", (node) => {
+        // Check if link is external by checking if the "url" attribute starts with http.
+        if (node.url.startsWith("http")) {
+          if (!node.data) {
+            // hProperties refers to the HTML attributes of the node in question.
+            // Ensure this object is added to the node.
+            node.data = { hProperties: {} }
+          }
+          // Assign the 'target' attribute.
+          node.data.hProperties = Object.assign({}, node.data.hProperties, {
+            target: "_blank"
+          })
+        }
+      })
+    )
     .processSync(markdown)
     .toString()
