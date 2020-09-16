7cca7a065a1f7464e1d3a8acea1ec4cc13f8b539
Jeff Lay <phritolay@icloud.com>
2020-09-16 13:51:15 -0400

    add className to layout
    related #148

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import classNames from "classnames"
 
 import Footer from "./footer"
 import Header from "./header"
@@ -8,14 +9,20 @@ import { DebugMediaQueries } from "@plugins/gatsby-ample-debuggers"
 import footer_test_data from "./footer/fixtures"
 import header_fixture from "./header/navigation/fixtures"
 
-const Layout = ({ children }) => (
+const Layout = ({ children, className }) => (
   <>
     <Header
       main_navigation={header_fixture.main_navigation}
       top_navigation={header_fixture.top_navigation}
     />
 
-    <main>{children}</main>
+    <main
+      className={classNames({
+        [className]: className
+      })}
+    >
+      {children}
+    </main>
 
     <Footer copyright="2020, All Rights Reserved" {...footer_test_data.default} />
 
@@ -24,7 +31,11 @@ const Layout = ({ children }) => (
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  /**
+   * Additional classes on the main element.
+   */
+  className: PropTypes.string
 }
 
 export default Layout
