c135049eb0cca6d767da205c184ca7abae3d633d
Sean C Davis <scdavis41@gmail.com>
2020-09-15 20:17:30 -0400

    Move button transformer into the component's directory

--- src/layout/container/transformers/index.js
+++ src/layout/container/transformers/index.js
@@ -1,9 +1,7 @@
-import button from "./component-button"
 import image from "./component-image"
 import form from "./component-form"
 
 export default {
-  "component-button": button,
   "component-image": image,
   "component-form": form
 }
