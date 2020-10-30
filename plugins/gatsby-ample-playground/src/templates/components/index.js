e2a13553da6b5c81c5d36558df24a1367efe11dc
Jeff Lay <phritolay@icloud.com>
2020-10-30 14:38:37 -0400

    Add toggle theme button
    related #204

--- plugins/gatsby-ample-playground/src/templates/components/index.js
+++ plugins/gatsby-ample-playground/src/templates/components/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import startCase from "lodash/startCase"
 import toLower from "lodash/toLower"
+import classNames from "classnames"
 import useDarkMode from "use-dark-mode"
 
 import Variations from "../../components/variations"
@@ -11,7 +12,7 @@ import config from "@root/playground.config"
 import Image from "@src/components/image"
 import Link from "@src/components/link"
 
-import icon from "@src/images/favicon.png"
+import favicon from "@src/images/favicon.png"
 
 import styles from "./styles.module.scss"
 
@@ -38,6 +39,20 @@ const ComponentsPlayground = () => {
 
   const title = `Components | ${config.title || "Ample Playground"}`
 
+  let toggleThemeIcon = (
+    <svg viewBox="0 0 512 512">
+      <path d="M256 143.7c-61.8 0-112 50.3-112 112.1s50.2 112.1 112 112.1 112-50.3 112-112.1-50.2-112.1-112-112.1zm0 192.2c-44.1 0-80-35.9-80-80.1s35.9-80.1 80-80.1 80 35.9 80 80.1-35.9 80.1-80 80.1zm256-80.1c0-5.3-2.7-10.3-7.1-13.3L422 187l19.4-97.9c1-5.2-.6-10.7-4.4-14.4-3.8-3.8-9.1-5.5-14.4-4.4l-97.8 19.4-55.5-83c-6-8.9-20.6-8.9-26.6 0l-55.5 83-97.8-19.5c-5.3-1.1-10.6.6-14.4 4.4-3.8 3.8-5.4 9.2-4.4 14.4L90 187 7.1 242.5c-4.4 3-7.1 8-7.1 13.3 0 5.3 2.7 10.3 7.1 13.3L90 324.6l-19.4 97.9c-1 5.2.6 10.7 4.4 14.4 3.8 3.8 9.1 5.5 14.4 4.4l97.8-19.4 55.5 83c3 4.5 8 7.1 13.3 7.1s10.3-2.7 13.3-7.1l55.5-83 97.8 19.4c5.4 1.2 10.7-.6 14.4-4.4 3.8-3.8 5.4-9.2 4.4-14.4L422 324.6l82.9-55.5c4.4-3 7.1-8 7.1-13.3zm-116.7 48.1c-5.4 3.6-8 10.1-6.8 16.4l16.8 84.9-84.8-16.8c-6.6-1.4-12.8 1.4-16.4 6.8l-48.1 72-48.1-71.9c-3-4.5-8-7.1-13.3-7.1-1 0-2.1.1-3.1.3l-84.8 16.8 16.8-84.9c1.2-6.3-1.4-12.8-6.8-16.4l-71.9-48.1 71.9-48.2c5.4-3.6 8-10.1 6.8-16.4l-16.8-84.9 84.8 16.8c6.5 1.3 12.8-1.4 16.4-6.8l48.1-72 48.1 72c3.6 5.4 9.9 8.1 16.4 6.8l84.8-16.8-16.8 84.9c-1.2 6.3 1.4 12.8 6.8 16.4l71.9 48.2-71.9 48z" />
+    </svg>
+  )
+
+  if (darkMode.value) {
+    toggleThemeIcon = (
+      <svg viewBox="0 0 512 512">
+        <path d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 00283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z" />
+      </svg>
+    )
+  }
+
   return (
     <main className={styles.playground}>
       <Helmet>
@@ -46,9 +61,17 @@ const ComponentsPlayground = () => {
 
       <header className={styles.playground_header}>
         <Link to="/" className={styles.logo}>
-          <Image src={icon} alt="logo" />
+          <Image src={favicon} alt="logo" />
         </Link>
         <h1>{title}</h1>
+
+        <button
+          className={classNames(styles.toggle_theme, { [styles[`dark_mode`]]: darkMode.value })}
+          type="button"
+          onClick={darkMode.toggle}
+        >
+          {toggleThemeIcon}
+        </button>
       </header>
 
       {components}
