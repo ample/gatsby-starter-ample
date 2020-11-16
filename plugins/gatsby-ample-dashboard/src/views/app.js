b5a3ca7a173b9f31367d9c77e2a5607ee3ea6d83
Sean C Davis <scdavis41@gmail.com>
2020-11-16 15:33:29 -0500

    Get refresh tokens working

--- plugins/gatsby-ample-dashboard/src/views/app.js
+++ plugins/gatsby-ample-dashboard/src/views/app.js
@@ -9,6 +9,8 @@ const DashboardAppWrapper = () => {
       domain={process.env.GATSBY_AUTH0_DOMAIN}
       clientId={process.env.GATSBY_AUTH0_CLIENT_ID}
       redirectUri={process.env.GATSBY_AUTH0_CALLBACK}
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
     >
       <Layout />
     </Auth0Provider>
--- plugins/gatsby-ample-dashboard/src/views/login/component.js
+++ plugins/gatsby-ample-dashboard/src/views/login/component.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import { navigate } from "gatsby"
 import { useAuth0 } from "@auth0/auth0-react"
 
