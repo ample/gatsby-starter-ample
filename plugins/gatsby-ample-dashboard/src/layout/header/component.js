b600e5092d4915c7d6b68bf910442d8fc469aec1
Sean C Davis <scdavis41@gmail.com>
2020-12-15 16:17:53 -0500

    Add a few styles for the header

--- plugins/gatsby-ample-dashboard/src/layout/header/component.js
+++ plugins/gatsby-ample-dashboard/src/layout/header/component.js
@@ -3,19 +3,30 @@ import { useAuth0 } from "@auth0/auth0-react"
 
 import styles from "./styles.module.scss"
 
+const isActive = href => {
+  if (typeof window !== "object" || !window.location) {
+    return false
+  }
+  return window.location.pathname === href
+}
+
 const LoggedInHeader = () => {
   const { logout } = useAuth0()
 
   return (
-    <div className={styles.header}>
-      <div className="container">
-        <a href="/admin">Dashboard</a>
-        <a href="/admin/training">Training</a>
-        <a href="/admin/sitemap">Sitemap</a>
-        <button onClick={() => logout({ returnTo: process.env.GATSBY_AUTH0_CALLBACK })}>
-          Sign Out
-        </button>
-      </div>
+    <div className={styles.logged_in_menu}>
+      <a href="/admin" className={isActive("/admin") ? styles.active : null}>
+        Dashboard
+      </a>
+      <a href="/admin/training" className={isActive("/admin/training") ? styles.active : null}>
+        Training
+      </a>
+      <a href="/admin/sitemap" className={isActive("/admin/sitemap") ? styles.active : null}>
+        Sitemap
+      </a>
+      <button onClick={() => logout({ returnTo: process.env.GATSBY_AUTH0_CALLBACK })}>
+        Sign Out
+      </button>
     </div>
   )
 }
@@ -24,10 +35,8 @@ const LoggedOutHeader = () => {
   const { loginWithRedirect } = useAuth0()
 
   return (
-    <div className={styles.header}>
-      <div className="container">
-        <button onClick={() => loginWithRedirect()}>Sign In (Auth0)</button>
-      </div>
+    <div className={styles.logged_out_menu}>
+      <button onClick={() => loginWithRedirect()}>Sign In (Auth0)</button>
     </div>
   )
 }
@@ -35,7 +44,11 @@ const LoggedOutHeader = () => {
 const Header = () => {
   const { isAuthenticated } = useAuth0()
 
-  return <div>{isAuthenticated ? <LoggedInHeader /> : <LoggedOutHeader />}</div>
+  return (
+    <div className={styles.header}>
+      <div className="container">{isAuthenticated ? <LoggedInHeader /> : <LoggedOutHeader />}</div>
+    </div>
+  )
 }
 
 export default Header
