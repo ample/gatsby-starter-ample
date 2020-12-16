f2d87a2cb3d0353d29bf88491e14e4206aa233f5
Sean C Davis <scdavis41@gmail.com>
2020-12-16 11:32:14 -0500

    Fix specs

--- plugins/gatsby-remark-ample/utils/process-frontmatter.test.js
+++ plugins/gatsby-remark-ample/utils/process-frontmatter.test.js
@@ -11,38 +11,24 @@ describe("processFrontmatter", () => {
   it("passes non-processable properties through", () => {
     const fm = { hello: "world" }
     const result = processFrontmatter({ frontmatter: fm, options: options })
-    expect(result).toEqual({ frontmatter: fm })
-  })
-  it("extracts seoData and deletes the key from frontmatter", () => {
-    const fm = { seo: { title: "Hello World" }, title: "Hello World" }
-    const result = processFrontmatter({ frontmatter: fm, options: options })
-    expect(result).toEqual({
-      frontmatter: { title: "Hello World" },
-      seoData: { title: "Hello World" }
-    })
+    expect(result).toEqual(fm)
   })
   it("processes markdown fields", () => {
     const fm = { test_md: "Hello world" }
     const result = processFrontmatter({ frontmatter: fm, options: options })
-    expect(result).toEqual({
-      frontmatter: { test_md: "Hello world", test: "<p>Hello world</p>\n" }
-    })
+    expect(result).toEqual({ test_md: "Hello world", test: "<p>Hello world</p>\n" })
   })
   it("processes nested markdown fields", () => {
     const fm = { a: { b: [{ test_md: "Hello world" }] } }
     const result = processFrontmatter({ frontmatter: fm, options: options })
-    expect(result).toEqual({
-      frontmatter: { a: { b: [{ test_md: "Hello world", test: "<p>Hello world</p>\n" }] } }
-    })
+    expect(result).toEqual({ a: { b: [{ test_md: "Hello world", test: "<p>Hello world</p>\n" }] } })
   })
   it("processes image fields", () => {
     const options = getOptions({ imageSrc: imageSrcDir })
     const fm = { test_src: "/images/pto.png" }
     const node = { fileAbsolutePath: absoluteFilePath }
     const result = processFrontmatter({ frontmatter: fm, options: options, node: node })
-    expect(result).toEqual({
-      frontmatter: { test_src: "/images/pto.png", test: "../images/pto.png" }
-    })
+    expect(result).toEqual({ test_src: "/images/pto.png", test: "../images/pto.png" })
   })
   it("processes nested image fields", () => {
     const options = getOptions({ imageSrc: imageSrcDir })
@@ -50,7 +36,7 @@ describe("processFrontmatter", () => {
     const node = { fileAbsolutePath: absoluteFilePath }
     const result = processFrontmatter({ frontmatter: fm, options: options, node: node })
     expect(result).toEqual({
-      frontmatter: { a: { b: [{ test_src: "/images/pto.png", test: "../images/pto.png" }] } }
+      a: { b: [{ test_src: "/images/pto.png", test: "../images/pto.png" }] }
     })
   })
 })
