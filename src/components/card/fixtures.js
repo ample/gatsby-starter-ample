f0d7ed7c82c59f16fd24a3cefbc91e6a789e47bd
Jeff Lay <phritolay@icloud.com>
2021-01-12 13:04:48 -0500

    add cards with images

--- src/components/card/fixtures.js
+++ src/components/card/fixtures.js
@@ -44,5 +44,46 @@ export default {
     image: "/uploads/blueprint/graphic-1.svg",
     textAlignment: "center",
     theme: "graphic"
+  },
+  with_image: {
+    body:
+      "<p>Cras mattis consectetur purus sit amet fermentum. Curabitur blandit tempus porttitor. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>",
+    button: {
+      label: "Read more",
+      theme: "arrow",
+      url: "#"
+    },
+    heading: "Card Title",
+    image: {
+      childImageSharp: {
+        fluid: {
+          aspectRatio: 1.5,
+          sizes: "(max-width: 1400px) 100vw, 1400px",
+          src: "/uploads/blueprint/rectangle.jpg"
+        }
+      }
+    },
+    theme: "image"
+  },
+  with_image_centered: {
+    body:
+      "<p>Cras mattis consectetur purus sit amet fermentum. Curabitur blandit tempus porttitor. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>",
+    button: {
+      label: "Read more",
+      theme: "arrow",
+      url: "#"
+    },
+    heading: "Card Title",
+    image: {
+      childImageSharp: {
+        fluid: {
+          aspectRatio: 1.5,
+          sizes: "(max-width: 1400px) 100vw, 1400px",
+          src: "/uploads/blueprint/rectangle.jpg"
+        }
+      }
+    },
+    textAlignment: "center",
+    theme: "image"
   }
 }
