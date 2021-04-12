45a4559e8ef0aeea28d59cfc8286b7c5b2ad599e
Dan Holloran <dan@helloample.com>
2021-04-12 16:29:59 -0400

    Fixes image fixture path for preview
    Signed-off-by: Dan Holloran <dan@helloample.com>

--- src/components/image/fixtures.js
+++ src/components/image/fixtures.js
@@ -7,15 +7,15 @@ export default {
           backgroundColor: "#d8e8f8",
           images: {
             fallback: {
-              src: "/static/9e01fe059bae0bedd117446c113abf11/b44b0/large-dark.jpg",
+              src: "/uploads/storybook/9e01fe059bae0bedd117446c113abf11/b44b0/large-dark.jpg",
               srcSet:
-                "/static/9e01fe059bae0bedd117446c113abf11/f782f/large-dark.jpg 250w,\n/static/9e01fe059bae0bedd117446c113abf11/24d85/large-dark.jpg 500w,\n/static/9e01fe059bae0bedd117446c113abf11/b44b0/large-dark.jpg 1000w,\n/static/9e01fe059bae0bedd117446c113abf11/8ba2d/large-dark.jpg 2000w",
+                "/uploads/storybook/9e01fe059bae0bedd117446c113abf11/f782f/large-dark.jpg 250w,\n/uploads/storybook/9e01fe059bae0bedd117446c113abf11/24d85/large-dark.jpg 500w,\n/uploads/storybook/9e01fe059bae0bedd117446c113abf11/b44b0/large-dark.jpg 1000w,\n/uploads/storybook/9e01fe059bae0bedd117446c113abf11/8ba2d/large-dark.jpg 2000w",
               sizes: "(min-width: 1000px) 1000px, 100vw"
             },
             sources: [
               {
                 srcSet:
-                  "/static/9e01fe059bae0bedd117446c113abf11/2a4db/large-dark.webp 250w,\n/static/9e01fe059bae0bedd117446c113abf11/c0386/large-dark.webp 500w,\n/static/9e01fe059bae0bedd117446c113abf11/e8e37/large-dark.webp 1000w,\n/static/9e01fe059bae0bedd117446c113abf11/9dca3/large-dark.webp 2000w",
+                  "/uploads/storybook/9e01fe059bae0bedd117446c113abf11/2a4db/large-dark.webp 250w,\n/uploads/storybook/9e01fe059bae0bedd117446c113abf11/c0386/large-dark.webp 500w,\n/uploads/storybook/9e01fe059bae0bedd117446c113abf11/e8e37/large-dark.webp 1000w,\n/uploads/storybook/9e01fe059bae0bedd117446c113abf11/9dca3/large-dark.webp 2000w",
                 type: "image/webp",
                 sizes: "(min-width: 1000px) 1000px, 100vw"
               }
@@ -28,7 +28,7 @@ export default {
     }
   },
   native: {
-    src: "/static/9e01fe059bae0bedd117446c113abf11/b44b0/large-dark.jpg",
+    src: "/uploads/storybook/9e01fe059bae0bedd117446c113abf11/b44b0/large-dark.jpg",
     width: "1077",
     height: "718"
   }
