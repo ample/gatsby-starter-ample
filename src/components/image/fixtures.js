9d7aa2734e04b1705eada70d0be5e29c9feb4841
Dan Holloran <dan@helloample.com>
2021-04-12 16:08:32 -0400

    Adds <Image /> story
    Signed-off-by: Dan Holloran <dan@helloample.com>

--- src/components/image/fixtures.js
+++ src/components/image/fixtures.js
@@ -4,32 +4,32 @@ export default {
       childImageSharp: {
         gatsbyImageData: {
           layout: "constrained",
-          backgroundColor: "#383848",
+          backgroundColor: "#d8e8f8",
           images: {
             fallback: {
-              src:
-                "/static/b5fd9b17fec90feb34f6e604f6f30791/1b035/MIbCzcvxQdahamZSNQ26_12082014-IMG_3526.jpg",
+              src: "/static/9e01fe059bae0bedd117446c113abf11/b44b0/large-dark.jpg",
               srcSet:
-                "/static/b5fd9b17fec90feb34f6e604f6f30791/99f43/MIbCzcvxQdahamZSNQ26_12082014-IMG_3526.jpg 250w,\n/static/b5fd9b17fec90feb34f6e604f6f30791/26cfb/MIbCzcvxQdahamZSNQ26_12082014-IMG_3526.jpg 500w,\n/static/b5fd9b17fec90feb34f6e604f6f30791/1b035/MIbCzcvxQdahamZSNQ26_12082014-IMG_3526.jpg 1000w,\n/static/b5fd9b17fec90feb34f6e604f6f30791/f19bc/MIbCzcvxQdahamZSNQ26_12082014-IMG_3526.jpg 2000w",
+                "/static/9e01fe059bae0bedd117446c113abf11/f782f/large-dark.jpg 250w,\n/static/9e01fe059bae0bedd117446c113abf11/24d85/large-dark.jpg 500w,\n/static/9e01fe059bae0bedd117446c113abf11/b44b0/large-dark.jpg 1000w,\n/static/9e01fe059bae0bedd117446c113abf11/8ba2d/large-dark.jpg 2000w",
               sizes: "(min-width: 1000px) 1000px, 100vw"
             },
             sources: [
               {
                 srcSet:
-                  "/static/b5fd9b17fec90feb34f6e604f6f30791/48571/MIbCzcvxQdahamZSNQ26_12082014-IMG_3526.webp 250w,\n/static/b5fd9b17fec90feb34f6e604f6f30791/921bf/MIbCzcvxQdahamZSNQ26_12082014-IMG_3526.webp 500w,\n/static/b5fd9b17fec90feb34f6e604f6f30791/b7ef5/MIbCzcvxQdahamZSNQ26_12082014-IMG_3526.webp 1000w,\n/static/b5fd9b17fec90feb34f6e604f6f30791/c9c6a/MIbCzcvxQdahamZSNQ26_12082014-IMG_3526.webp 2000w",
+                  "/static/9e01fe059bae0bedd117446c113abf11/2a4db/large-dark.webp 250w,\n/static/9e01fe059bae0bedd117446c113abf11/c0386/large-dark.webp 500w,\n/static/9e01fe059bae0bedd117446c113abf11/e8e37/large-dark.webp 1000w,\n/static/9e01fe059bae0bedd117446c113abf11/9dca3/large-dark.webp 2000w",
                 type: "image/webp",
                 sizes: "(min-width: 1000px) 1000px, 100vw"
               }
             ]
           },
           width: 1000,
-          height: 667
+          height: 414.99999999999994
         }
       }
     }
   },
   native: {
-    src:
-      "https://images.unsplash.com/44/MIbCzcvxQdahamZSNQ26_12082014-IMG_3526.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format"
+    src: "/static/9e01fe059bae0bedd117446c113abf11/b44b0/large-dark.jpg",
+    width: "1077",
+    height: "718"
   }
 }
